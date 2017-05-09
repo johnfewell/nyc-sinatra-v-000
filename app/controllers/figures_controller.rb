61ab7703753b3445d2fc5771e8608ecd80e4000d
John Fewell <fewell@gmail.com>
2017-05-09 20:42:45 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,8 +1,22 @@
 class FiguresController < ApplicationController
 
-  get '/figures/new' do
+  get '/figures' do
     @figures = Figure.all
     erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
   end
 
   post '/figures' do
@@ -11,14 +25,27 @@ class FiguresController < ApplicationController
      @figure.landmarks << Landmark.create(params[:landmark])
    end
 
+
+
    if !params[:title][:name].empty?
      @figure.titles << Title.create(params[:title])
    end
 
    @figure.save
-   redirect to "/figures/#{@figure.id}"
- end
+     redirect to "/figures/#{@figure.id}"
+   end
 
+   post '/figures/:id/' do
+     @figure = Figure.find(params[:id])
+     @figure.update(params[:figure])
+
+     if !params[:landmark][:name].empty?
+       @figure.titles << Title.create(params[:title])
+     end
+     @figure.save
+     redirect to "/figures/#{@figure.id}"
+
+   end
 
 
 # pass it back to each of the objects and via their params and then save each objects
